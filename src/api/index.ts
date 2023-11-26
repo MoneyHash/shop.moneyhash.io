1fa91352c5263b516807a23d49fe1bb4668d5bb9
Mo'men Sherif <momensherif.2019@gmail.com>
2023-11-27 00:14:47 +0200

    chore: restore staging env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://staging-web.moneyhash.io/api/v1.1',
-  baseURL: 'http://localhost:8000/api/v1.1',
+  baseURL: 'https://staging-web.moneyhash.io/api/v1.1',
 });
 
 axiosInstance.interceptors.response.use(res => res.data);
 
-// const ACCOUNT_API_KEY = 'PzNEWbYv.D3Mfra0jsdy7zrJYHyhl68XF5vIlmvA3';
-const ACCOUNT_API_KEY = '7vC1gP2c.CY54djhP7Oac5vYCvjnA1FTySIkjy34g';
+const ACCOUNT_API_KEY = 'PzNEWbYv.D3Mfra0jsdy7zrJYHyhl68XF5vIlmvA3';
 
 axiosInstance.interceptors.request.use(config => {
   // eslint-disable-next-line no-param-reassign
