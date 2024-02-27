d4539c8aa03686ab935db11c0f1e250c9e6a25e3
Mo'men Sherif <momensherif.2019@gmail.com>
2024-02-27 19:39:31 +0200

    chore: update local api key

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,8 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.response.use(res => res.data);
 
-const ACCOUNT_API_KEY = 'KpTgel7x.F8wDAPinQRCRjlfIkShohBJC89F5Yakh';
+// const ACCOUNT_API_KEY = 'KpTgel7x.F8wDAPinQRCRjlfIkShohBJC89F5Yakh';
+const ACCOUNT_API_KEY = '5e8ZrVFB.oSpd8VTKBKyZqx4HGuNNhhhzPC0tbAFb';
 
 axiosInstance.interceptors.request.use(config => {
   // eslint-disable-next-line no-param-reassign
