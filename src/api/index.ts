02830c3beea056fab24ac9de898f1d411e7b83cc
Mo'men Sherif <momensherif.2019@gmail.com>
2024-05-08 20:42:40 +0300

    chore: overrride user agent to be chrome windows

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,14 +2,14 @@ import axios from 'axios';
 import { isSafari } from '@braintree/browser-detection';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://staging-web.moneyhash.io/api/v1.1', // staging
-  // baseURL: 'https://web.moneyhash.io/api/v1.1', // TODO: production
+  // baseURL: 'https://staging-web.moneyhash.io/api/v1.1', // staging
+  baseURL: 'https://web.moneyhash.io/api/v1.1', // TODO: production
 });
 
 axiosInstance.interceptors.response.use(res => res.data);
 
-const ACCOUNT_API_KEY = 'lMEv4X5O.gu7Eo6vYYeiNkeY6rWogqe8fXSkcruva'; // staging
-// const ACCOUNT_API_KEY = 'Fe0mKMmc.DzQA3Jb4ehjAVyBP64O88LHfDqcQ8hjB'; // TODO: production
+// const ACCOUNT_API_KEY = 'lMEv4X5O.gu7Eo6vYYeiNkeY6rWogqe8fXSkcruva'; // staging
+const ACCOUNT_API_KEY = 'Fe0mKMmc.DzQA3Jb4ehjAVyBP64O88LHfDqcQ8hjB'; // TODO: production
 
 axiosInstance.interceptors.request.use(config => {
   // eslint-disable-next-line no-param-reassign
