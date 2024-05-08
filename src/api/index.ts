53a60637255956ecdd2db317bdddbe7e284cd73c
Mo'men Sherif <momensherif.2019@gmail.com>
2024-05-08 10:31:24 +0300

    chore: use decahtlon creds

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://staging-web.moneyhash.io/api/v1.1', // staging
-  baseURL: 'https://web.moneyhash.io/api/v1.1', // TODO: production
+  baseURL: 'https://staging-web.moneyhash.io/api/v1.1', // staging
+  // baseURL: 'https://web.moneyhash.io/api/v1.1', // TODO: production
 });
 
 axiosInstance.interceptors.response.use(res => res.data);
 
 // const ACCOUNT_API_KEY = 'lMEv4X5O.gu7Eo6vYYeiNkeY6rWogqe8fXSkcruva'; // staging
-const ACCOUNT_API_KEY = 'Fe0mKMmc.DzQA3Jb4ehjAVyBP64O88LHfDqcQ8hjB'; // TODO: production
+// const ACCOUNT_API_KEY = 'Fe0mKMmc.DzQA3Jb4ehjAVyBP64O88LHfDqcQ8hjB'; // TODO: production
+const ACCOUNT_API_KEY = 'KpTgel7x.F8wDAPinQRCRjlfIkShohBJC89F5Yakh'; // Decathlon
 
 axiosInstance.interceptors.request.use(config => {
   // eslint-disable-next-line no-param-reassign
