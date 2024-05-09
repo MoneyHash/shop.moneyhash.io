f0f7080b5c396a2f1a4c8e692ed499d2a7cee7a6
Mo'men Sherif <momensherif.2019@gmail.com>
2024-05-09 10:29:27 +0300

    revert: override user agent

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { isSafari } from '@braintree/browser-detection';
 
 const axiosInstance = axios.create({
   // baseURL: 'https://staging-web.moneyhash.io/api/v1.1', // staging
@@ -14,13 +13,6 @@ const ACCOUNT_API_KEY = 'Fe0mKMmc.DzQA3Jb4ehjAVyBP64O88LHfDqcQ8hjB'; // TODO: pr
 axiosInstance.interceptors.request.use(config => {
   // eslint-disable-next-line no-param-reassign
   config.headers['x-api-key'] = ACCOUNT_API_KEY;
-  if (!isSafari()) {
-    // eslint-disable-next-line no-console
-    console.log('Not Safari override user agent');
-    // eslint-disable-next-line no-param-reassign
-    config.headers['User-Agent'] =
-      'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36';
-  }
   return config;
 });
 
