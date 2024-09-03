d3a3ce9604be535247b799bc58b868ad0bd451c2
Mo'men Sherif <momensherif.2019@gmail.com>
2024-09-03 19:33:27 +0300

    chore: debug popup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { IntentStateDetails } from '@moneyhash/js-sdk';
 import App from './App';
 import './index.css';
 
@@ -8,6 +10,7 @@ import Home from './pages/home';
 import Checkout from './pages/checkout';
 import Order from './pages/order';
 import IntegrationGuide from './pages/integrationGuide';
+import useMoneyHash from './hooks/useMoneyHash';
 
 const router = createBrowserRouter([
   {
@@ -30,10 +33,52 @@ const router = createBrowserRouter([
         path: '/integration-guide',
         element: <IntegrationGuide />,
       },
+      {
+        path: '/playground',
+        element: <Playground />,
+      },
     ],
   },
 ]);
 
+function Playground() {
+  const [intentId, setIntentId] = useState('');
+  const moneyHash = useMoneyHash({
+    onComplete: data => {
+      console.log('Complete', data);
+    },
+    onFail: data => {
+      console.log('Fail', data);
+    },
+  });
+
+  return (
+    <div>
+      <h1>Playground</h1>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Intent ID"
+        value={intentId}
+        onChange={e => setIntentId(e.target.value)}
+      />
+      <button
+        type="button"
+        onClick={async () => {
+          const { stateDetails } = await moneyHash.getIntentDetails(intentId);
+          moneyHash.renderUrl({
+            intentId,
+            url: (stateDetails as IntentStateDetails<'URL_TO_RENDER'>)?.url,
+            renderStrategy: 'POPUP_IFRAME',
+          });
+        }}
+      >
+        Click me
+      </button>
+    </div>
+  );
+}
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
