12628ee35a74016f5d52b249e749979cbbb64823
Mo'men Sherif <momensherif.2019@gmail.com>
2025-01-08 16:31:21 +0200

    chore: test loader customization on redirection

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,21 +2,21 @@ import axios from 'axios';
 import safeLocalStorage from '@/utils/safeLocalStorage';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://web.moneyhash.io/api/v1.1',
-  // baseURL: 'https://staging-web.moneyhash.io/api/v1.1',
+  // baseURL: 'https://web.moneyhash.io/api/v1.1',
+  baseURL: 'https://staging-web.moneyhash.io/api/v1.1',
 });
 
 axiosInstance.interceptors.response.use(res => res.data);
 
 // Production API key
-const ACCOUNT_API_KEY = 'NMyQeKCE.PE1ibNHTXepIxg0hyYrmU4LzK4sNdUS1';
-export const ACCOUNT_PUBLIC_API_KEY =
-  'public.WsCZwBVz.mUyakj73ByciUGMOE1UYvGSFDJC7uu6ftLs4C5fy';
+// const ACCOUNT_API_KEY = 'NMyQeKCE.PE1ibNHTXepIxg0hyYrmU4LzK4sNdUS1';
+// export const ACCOUNT_PUBLIC_API_KEY =
+//   'public.WsCZwBVz.mUyakj73ByciUGMOE1UYvGSFDJC7uu6ftLs4C5fy';
 
 // Staging API key
-// const ACCOUNT_API_KEY = 'wocSeGMI.e3l92r5b9NYXVgTLfBXvED88oppdsi3H';
-// export const ACCOUNT_PUBLIC_API_KEY =
-//   'public.nFsXq2BS.rwzwRJAZaq8jEEPZcnMldOSFXIqklPOe9QXaOwW1';
+const ACCOUNT_API_KEY = 'wocSeGMI.e3l92r5b9NYXVgTLfBXvED88oppdsi3H';
+export const ACCOUNT_PUBLIC_API_KEY =
+  'public.nFsXq2BS.rwzwRJAZaq8jEEPZcnMldOSFXIqklPOe9QXaOwW1';
 
 axiosInstance.interceptors.request.use(config => {
   // eslint-disable-next-line no-param-reassign
