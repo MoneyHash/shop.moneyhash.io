649b98ec56fb9ad408f48d676abd2f000ac1b90e
Mo'men Sherif <momensherif.2019@gmail.com>
2025-05-12 14:36:47 +0300

    feat: switch environment functionality

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,36 @@
 import axios from 'axios';
 import safeLocalStorage from '@/utils/safeLocalStorage';
 
+const localEnv = (localStorage.getItem('env') || 'production') as
+  | 'staging'
+  | 'production';
+
+const config = {
+  staging: {
+    baseURL: 'https://staging-web.moneyhash.io/api/v1.1',
+    accountApiKey: 'wocSeGMI.e3l92r5b9NYXVgTLfBXvED88oppdsi3H',
+    accountPublicApiKey:
+      'public.nFsXq2BS.rwzwRJAZaq8jEEPZcnMldOSFXIqklPOe9QXaOwW1',
+  },
+  production: {
+    baseURL: 'https://web.moneyhash.io/api/v1.1',
+    accountApiKey: 'NMyQeKCE.PE1ibNHTXepIxg0hyYrmU4LzK4sNdUS1',
+    accountPublicApiKey:
+      'public.WsCZwBVz.mUyakj73ByciUGMOE1UYvGSFDJC7uu6ftLs4C5fy',
+  },
+} as const;
+
+const { baseURL, accountApiKey, accountPublicApiKey } = config[localEnv];
+
 const axiosInstance = axios.create({
-  baseURL: 'https://web.moneyhash.io/api/v1.1',
-  // baseURL: 'https://staging-web.moneyhash.io/api/v1.1',
+  baseURL,
 });
 
 axiosInstance.interceptors.response.use(res => res.data);
 
 // Production API key
-const ACCOUNT_API_KEY = 'NMyQeKCE.PE1ibNHTXepIxg0hyYrmU4LzK4sNdUS1';
-export const ACCOUNT_PUBLIC_API_KEY =
-  'public.WsCZwBVz.mUyakj73ByciUGMOE1UYvGSFDJC7uu6ftLs4C5fy';
-
-// Staging API key
-// const ACCOUNT_API_KEY = 'wocSeGMI.e3l92r5b9NYXVgTLfBXvED88oppdsi3H';
-// export const ACCOUNT_PUBLIC_API_KEY =
-//   'public.nFsXq2BS.rwzwRJAZaq8jEEPZcnMldOSFXIqklPOe9QXaOwW1';
+const ACCOUNT_API_KEY = accountApiKey;
+export const ACCOUNT_PUBLIC_API_KEY = accountPublicApiKey;
 
 axiosInstance.interceptors.request.use(config => {
   // eslint-disable-next-line no-param-reassign
